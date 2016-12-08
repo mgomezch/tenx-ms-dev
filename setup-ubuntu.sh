5d064f55982ebb50dc05f925babd0e1d23df4002
Manuel Gómez <targen@gmail.com>
2016-12-08 11:33:53 -0400

    Ubuntu setup: more comments, fix missing sudo in sed

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -47,6 +47,16 @@ sudo rm -f '/etc/dnsmasq.d/'{'docker','interfaces'}
 # DNS servers defined in /etc/resolv.conf at the time the dnsmasq server starts,
 # which makes it rely instead on its configuration files to find suitable back
 # ends for each query it receives.
+#
+# The bind-dynamic option makes the local dnsmasq instance bind separate sockets
+# for each network interface, which is generally preferred, for security, to a
+# single socket listening on the wildcard address that relies on dnsmasq to
+# exclude unwanted requests in userspace.  The bind-address option, which is
+# mutually exclusive with the bind-dynamic option, also enables this behavior,
+# but will additionally adjust its set of listening sockets automatically on
+# network interface status changes, so it can start listening automatically on
+# network interfaces enabled after the dnsmasq daemon starts.  This is desired
+# since Docker creates network interfaces dynamically.
 sudo tee '/etc/dnsmasq.d/basic' <<EOF
 no-resolv
 bind-dynamic
@@ -60,18 +70,37 @@ server=8.8.4.4
 EOF
 
 # If any dnsmasq configuration files enable the bind-interfaces option, it will
-# conclict with 
+# conclict with the bind-dynamic option specified in the basic configuration.
 for file in /etc/dnsmasq.d/*
 do
-  sed -i -e 's/^\s*bind-interfaces\s*\(#.*\)\?$/# &/' "${file}"
+  sudo sed -i -e 's/^\s*bind-interfaces\s*\(#.*\)\?$/# &/' "${file}"
 done
 
-# Set up local DNS for Channel VPN:
+# This configures the local DNS forwarding server to use certain specific back
+# end DNS servers for certain domain names.  Domains handled by private DNS
+# servers should be resolved using those instead of the public Internet DNS
+# infrastructure, but domains that can be resolved by the public DNS should not
+# be resolved with private servers, as those would only be available if a link
+# to the proper VPN is online.
+#
+# TODO: This file should be generated dynamically on connection to the Ten-X VPN
+# using the DNS IP addresses provided by DHCP.
 sudo tee '/etc/dnsmasq.d/channel-corp' <<EOF
+
+# VPN endpoints should be resolved through the public DNS, as they have to be
+# resolved before any VPN link is established.
 server=/cdm.channel-corp.com/8.8.8.8
 server=/r2.auction.com/8.8.8.8
 
+# Some specific domains are inside namespaces handled by private DNS servers but
+# can also be resolved using public DNS servers.  Public resolution is preferred
+# for these as they will not fail to resolve when VPN links are offline.
 server=/github.channel-corp.com/8.8.8.8
+
+# For any other domain in a namespace known to be handled by private DNS servers
+# inside a VPN, use the well-known private DNS server VPN IP addresses as back
+# ends for domain name resolution.  Note it is not possible to know the full set
+# of such domain names in advance, so these are just some well-known roots.
 
 server=/channel-corp.com/10.3.10.23
 server=/channel-corp.com/10.3.10.24
@@ -87,9 +116,11 @@ server=/auction.local/10.3.10.24
 
 address=/cs.enterprise.com/127.0.0.1
 address=/cd.enterprise.com/127.0.0.1
+
 EOF
 
 # Disable Network Manager dnsmasq instances:
+# TODO: expand comment
 sudo sed -i '/etc/NetworkManager/NetworkManager.conf' -e 's/^dns=dnsmasq$/#&/'
 sudo rm -f '/etc/dnsmasq.d/network-manager'
 sudo pkill -f 'dnsmasq.*NetworkManager'
@@ -97,14 +128,22 @@ sudo pkill -f 'dnsmasq.*NetworkManager'
 # Reload local DNS configuration:
 sudo service dnsmasq restart
 
-# Make name lookups for .local domains not go through mDNS as they should, and hit DNS instead, as domains in auction.local are expected to resolve through the Channel internal DNS servers (in violation of RFC 6762 — be warned this breaks mDNS):
+# Make name lookups for .local domains not go through mDNS as they should, and
+# hit DNS instead, as domains in auction.local are expected to resolve through
+# the Ten-X internal DNS servers.  This is necessary because the Ten-X internal
+# domain name allocation scheme violates RFC 6762 requirements by using the
+# reserved .local top-level domain for a purpose other than mDNS, but the Name
+# Service Switch configuration that comes by default with any reasonably modern
+# system will come configured to use mDNS for domains under the .local TLD.
+# Note that this may break mDNS and it may direct mDNS queries to public DNS,
+# which may have an undesirable privacy impact.
 sudo tee '/etc/nsswitch.conf' <<EOF
 passwd:         compat
 group:          compat
 shadow:         compat
 gshadow:        files
 
-hosts:          files dns [NOTFOUND=return] mdns4_minimal
+hosts:          files dns [NOTFOUND=continue] mdns4_minimal
 networks:       files
 
 protocols:      db files
@@ -116,6 +155,7 @@ netgroup:       nis
 EOF
 
 # Configure Docker daemon:
+# TODO: expand comment
 sudo tee '/etc/docker/daemon.json' <<EOF
 {
   "bip": "172.17.0.1/24",
